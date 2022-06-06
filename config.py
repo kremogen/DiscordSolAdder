75c43575cae93baf8087e3298992b2e790591ff4
Nikita <nik.kosachev@mail.ru>
2022-06-06 18:24:23 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,6 @@
+settings = {
+    'token': 'OTgzMzY1MDI1NDYyODkwNTM2.GzOhNX.8GN4q4y1OK6vWBNpVhHG_wNVipDTJCboKkwKCA',
+    'bot': 'SolanaWallet',
+    'id': 983365025462890536,
+    'prefix': '$'
+}
---
+++ main.py
@@ -0,0 +1,38 @@
+import discord
+from discord.ext import commands
+from config import settings
+import csv
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix=settings['prefix'], intents=intents)
+channelId = 749953407350997125
+
+
+@bot.event
+async def on_ready():
+    print('Я запущен!')
+
+
+async def write_to_csv(discord_id, solana_address):
+    exist = False
+    with open('Wallet_binding.csv', 'r+', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            if discord_id in row or solana_address in row:
+                exist = True
+                return False
+        if not exist:
+            writer = csv.writer(csvfile, delimiter=',')
+            writer.writerow((discord_id, solana_address))
+            return True
+        csvfile.close()
+
+@bot.event
+async def on_message(message):
+    if len(message.content) == 44 and message.channel.id == channelId and message.author != bot.user:
+            if await write_to_csv(str(message.author.id), message.content):
+                await message.channel.send(f'Успешно добавлен!')
+            else:
+                await message.channel.send(f'Ты уже добавил свой кошелёк!')
+
+bot.run(settings['token'])
