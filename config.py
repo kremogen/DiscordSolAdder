aef950242cf324abd5848031ee4b0ccac9294b2f
Nikita <43411544+kremogen@users.noreply.github.com>
2022-06-06 19:48:11 +0300

    Visual changes

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 settings = {
-    'token': 'OTgzMzY1MDI1NDYyODkwNTM2.GzOhNX.8GN4q4y1OK6vWBNpVhHG_wNVipDTJCboKkwKCA',
+    'token': 'token',
     'bot': 'SolanaWallet',
-    'id': 983365025462890536,
+    'id': id,
     'prefix': '$'
 }
